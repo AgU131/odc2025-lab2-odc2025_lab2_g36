07448c32773a82e1482c04682602f4b104bed23d
lelopax <162189141+lelopax@users.noreply.github.com>
2025-06-04 18:29:25 -0300

    Animación mover copa y nubes

--- ejercicio2/app.s
+++ ejercicio2/app.s
@@ -6,13 +6,15 @@
 	.equ GPIO_GPFSEL0,   0x00
 	.equ GPIO_GPLEV0,    0x34
 
+        .include "auxiliares.s"
+
 	.globl main
 
 main:
 	// x0 contiene la direccion base del framebuffer
  	mov x20, x0	// Guarda la dirección base del framebuffer en x20
 	//---------------- CODE HERE ------------------------------------
-
+/* ESTO PARA QUE PINTE EL FUCSIA lo saco por el momento.
 	movz x10, 0xC7, lsl 16
 	movk x10, 0x1585, lsl 00
 
@@ -26,7 +28,7 @@ loop0:
 	cbnz x1,loop0  // Si no terminó la fila, salto
 	sub x2,x2,1	   // Decrementar contador Y
 	cbnz x2,loop1  // Si no es la última fila, salto
-
+*/
 	// Ejemplo de uso de gpios
 	mov x9, GPIO_BASE
 
@@ -45,6 +47,135 @@ loop0:
 	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
 	// efectivamente, su valor representará si GPIO 2 está activo
 	lsr w11, w11, 1
+
+//--- DIBUJAMOS LA IMAGEN ---//
+bl dibujar_fondo
+
+
+
+//--- ANIMACIÓN --
+movz x15, 298, lsl 0
+movz x16, 250, lsl 0     // Posición inicial de la copa
+movz x29, 0, lsl 0       // Empezamos subiendo la copa
+movz x17, 60, lsl 0      // Posición inicial de la nube
+movz x18, 42, lsl 0
+
+bucle_animacion:
+
+    bl dibujar_fondo // llamamos a dibujar fondo en cada iteración para que no dejen "estela" aquello que se vaya a mover
+
+    // --- Restaurar posición base de nubes ---
+    mov x17, x21                     // x17 va a tomar la posicion base de x de las nubes
+    mov x18, x22                     // x18 va a tomar la posicion base de y de las nubes
+
+
+    // --- Empezamos a dibujar las nubes (las movemos de posicion con add o sub) ---
+    // --- Además en cada iteración, comparamos x17 (X de cada nube) con 640 para ver si llego al final de la pantalla, en caso de no haber llegado, dibuja la nube en la                   posición que haya en x17, en caso de haber llegado a 640, le restamos 700 a x17, para que la nube vuelva a aparecer por la izquierda de la pantalla. (el 700 es editable, consideramos que en 700 quedaba bien)
+
+    // --- Nube 1  ---
+    cmp x17, 640
+    b.lt nube1_dibujar
+    sub x17, x17, 700
+nube1_dibujar:
+    bl dibujar_nube
+
+    // --- Nube 2  ---
+    add x17, x17, 100
+    add x18, x18, 80
+    cmp x17, 640
+    b.lt nube2_dibujar   
+    sub x17, x17, 700
+nube2_dibujar:
+    bl dibujar_nube
+
+    // --- Nube 3  ---
+    add x17, x17, 150
+    sub x18, x18, 50
+    cmp x17, 640
+    b.lt nube3_dibujar
+    sub x17, x17, 700
+nube3_dibujar:
+    bl dibujar_nube
+
+    // --- Nube 4  ---
+    sub x17, x17, 350
+    add x18, x18, 50
+    cmp x17, 640
+    b.lt nube4_dibujar
+    sub x17, x17, 700
+nube4_dibujar:
+    bl dibujar_nube
+
+    // --- Nube 5  ---
+    add x17, x17, 450
+    cmp x17, 640
+    b.lt nube5_dibujar
+    sub x17, x17, 700
+nube5_dibujar:
+    bl dibujar_nube
+
+    // --- Nube 6 ---
+    sub x17, x17, 355
+    add x18, x18, 93
+    cmp x17, 640
+    b.lt nube6_dibujar
+    sub x17, x17, 700
+nube6_dibujar:
+    bl dibujar_nube
+
+    // --- Nube 7 ---
+    add x17, x17, 305
+    add x18, x18, 25
+    cmp x17, 640
+    b.lt nube7_dibujar
+    sub x17, x17, 700
+nube7_dibujar:
+    bl dibujar_nube
+
+    // --- Nube 8  ---
+    sub x17, x17, 100
+    sub x18, x18, 38
+    cmp x17, 640
+    b.lt nube8_dibujar
+    sub x17, x17, 700
+nube8_dibujar:
+    bl dibujar_nube
+
+    // --- Movimiento nubes ---
+    add x21, x21, 1      // las nubes apareceran 1 x a la derecha
+
+    // Para el rebote de la copa, buscamos un registro (x29) en donde podamos guardar si la copa esta subiendo (x29 = 0), o esta bajando (x29 = 1) , y a partir de ahi hacer    el ciclo
+
+    // --- Rebote de la copa ---
+    cmp x29, 0              // Compara si la copa esta subiendo
+    b.ne bajar_copa        // Si no es igual a 0, salta a bajar_copa, sino sigue normal
+
+    sub x16, x16, 1         // Resta 1 al eje Y de la copa, por lo tanto sube
+    cmp x16, 0              
+    b.gt continuar_copa     // Mientras x16 sea distinto de 0 (superior de la pantalla), salta a continuar_copa y sigue.
+    mov x29, 1              // Si llego al superior de la pantalla, queremos que empieze a bajar, por lo tanot ponemos x29 en 1
+    b continuar_copa
+
+bajar_copa:
+    add x16, x16, 1         // Empieza a bajar la copa
+    cmp x16, 240           // Fijamos 240 como el limite inferior hasta dnde queremos que baje, x16 sea menor que 240, va sibujando la copa en bajada
+    b.lt continuar_copa
+    mov x29, 0              // Llego al tope inferior, ponemos x29 en 0 para que sube
+    
+continuar_copa:
+    bl dibujar_copa
+
+    // --- Delay (se pueden editar las constantes para cambiar el tiempo de delay y la velocidad) ---
+    movz x27, 150, lsl 0
+    delay_externo:
+        movz x28, 50000, lsl 0
+    delay_interno:
+        subs x28, x28, 1
+        b.ne delay_interno
+        subs x27, x27, 1
+        b.ne delay_externo
+
+    b bucle_animacion
 
 	//---------------------------------------------------------------
 	// Infinite Loop
---
+++ ejercicio2/auxiliares.s
@@ -0,0 +1,613 @@
+.equ SCREEN_WIDTH, 		640
+.equ SCREEN_HEIGH, 		480
+
+//----FUNCIONES AUXILIARES----
+
+//usaremos x3 y x4 para las posiciones de X e Y respectivamente 
+
+//Funcion para calcular la posicion de los pixeles mediante la ecuacion dada en el archivo
+calc_pixel:
+// x3 = X
+// x4 = Y
+// x0 = posicion del pixel en la pantalla
+  mov x0, 640	        // x0 = 640
+  mul x0, x0, x4	// x0 = (640 * y)	
+  add x0, x0, x3	// x0 = (640 * y) + x
+  lsl x0, x0, 2		// x0 = ((640 * y) + x) * 4
+  add x0, x0, x20       // x0 = ((640 * y) + x) * 4 + (dirección de inicio)
+  ret
+
+//funcion para hacer rectangulos y dibujar. Para llamarla en otras funciones movemos los numeros que querramos en los registros correspondientes (x1,x2,x3,x4).
+rectangulo: 
+// x1 = ancho
+// x2 = largo
+// x3 = X
+// x4 = Y
+// w10 = color
+
+ SUB SP, SP, 40 										
+ STUR x30, [SP, 32]
+ STUR x13, [SP, 24]
+ STUR x12, [SP, 16]
+ STUR x11, [SP, 8]
+ STUR x9,  [SP, 0]
+
+ BL calc_pixel
+
+//iniciamos el ciclo de dibujo del rectangulo
+
+ mov x9, x2 // en x9 guardamos el alto del rertangulo
+ mov x11, x0 // guardamos en x11 el pixel a pintar
+ ciclo_filas:
+    mov x12, x1    //guardamos en x12 el ancho del rectangulo
+    mov x13, x11   //guardamos en x13 el pixel inicial de cada fila 
+    ciclo_columnas:
+         stur w10, [x11] //pintamos el pixel seleccionado
+         add x11, x11, 4 //avanzamos al siguiente pixel
+         sub x12, x12, 1 //restamos uhn pixel al contador del ancho
+         cbnz x12, ciclo_columnas
+         //seguimos pintando la misma fila mientras el contador sea distinto a 0
+         mov x11, x13             // volver al inicio de la fila
+         movz x14, 640, lsl 0     // SCREEN_WIDTH
+         lsl x14, x14, 2          // x14 = SCREEN_WIDTH * 4 bytes
+         add x11, x11, x14        // bajar una fila
+         sub x9, x9, 1           // restamos 1 al contador de altura
+         cbnz x9, ciclo_filas // mientras el alto del rectangulo sea disntinto de 0, seguimos pintando
+    // restauramos registros del stack
+    LDR x9,  [SP, 0]
+    LDR x11, [SP, 8]
+    LDR x12, [SP, 16]
+    LDR x13, [SP, 24]
+    LDR x30, [SP, 32]
+    ADD SP, SP, 40
+    ret
+
+dibujar_copa:
+
+//usamos x15 y x16 como posicion inicial para la copa, y luego usamos los add y sub para ir moviendonos entro los rectangulos de la copa y dibujar nuevos
+sub sp, sp, 8
+stur x30, [sp, 0]
+
+    // --- Centro Nivel 1 ---
+    movz x1, 30, lsl 0
+    movz x2, 30, lsl 0
+    mov x3, x15
+    mov x4, x16
+    movz w10, 0xA520, lsl 0
+    movk w10, 0x00DA, lsl 16
+    bl rectangulo
+
+    // --- Centro Nivel 2 ---
+    movz x1, 22, lsl 0
+    movz x2, 38, lsl 0
+    add x3, x15, #4
+    sub x4, x16, #3
+    bl rectangulo
+
+    // --- Centro Nivel 3 ---
+    movz x1, 38, lsl 0
+    movz x2, 20, lsl 0
+    sub x3, x15, #4
+    add x4, x16, #5
+    bl rectangulo
+
+    // --- Brillo ---
+    movz x1, 6, lsl 0
+    movz x2, 4, lsl 0
+    add x3, x15, #22
+    add x4, x16, #7
+    movz w10, 0xE680, lsl 0
+    movk w10, 0x00FF, lsl 16
+    bl rectangulo
+
+    // --- Centro más fino ---
+    movz x1, 14, lsl 0
+    movz x2, 16, lsl 0
+    add x3, x15, #8
+    add x4, x16, #26
+    movz w10, 0xA520, lsl 0
+    movk w10, 0x00DA, lsl 16
+    bl rectangulo
+
+    // --- Tronco fino ---
+    movz x1, 10, lsl 0
+    movz x2, 70, lsl 0
+    add x3, x15, #10
+    mov x4, x16
+    bl rectangulo
+
+    // --- Tronco base ---
+    movz x1, 18, lsl 0
+    movz x2, 8, lsl 0
+    add x3, x15, #6
+    add x4, x16, #55
+    bl rectangulo
+
+    // --- Base verde arriba ---
+    movz x1, 24, lsl 0
+    movz x2, 4, lsl 0
+    add x3, x15, #3
+    add x4, x16, #63
+    movz w10, 0x5000, lsl 0
+    movk w10, 0x0000, lsl 16
+    bl rectangulo
+
+    // --- Base medio ---
+    movz x1, 28, lsl 0
+    movz x2, 4, lsl 0
+    add x3, x15, #1
+    add x4, x16, #67
+    movz w10, 0xA520, lsl 0
+    movk w10, 0x00DA, lsl 16
+    bl rectangulo
+
+    // --- Base abajo ---
+    movz x1, 32, lsl 0
+    movz x2, 5, lsl 0
+    sub x3, x15, #1
+    add x4, x16, #71
+    movz w10, 0x5000, lsl 0
+    movk w10, 0x0000, lsl 16
+    bl rectangulo
+
+ldr x30, [sp, 0]
+add sp, sp, 8
+    ret
+
+
+dibujar_nube:
+
+        sub sp, sp, 8
+        stur x30, [sp, 0]
+	// Dibuja una nube tomando (x17, x18), lo cual facilita la animacion (cambio de registros para que no se solapen con la copa)
+
+	mov x1, 60
+	mov x2, 10
+	mov x3, x17
+	mov x4, x18
+	movz w10, 0xD0D0, lsl 0
+	movk w10, 0x00D0, lsl 16
+	bl rectangulo
+
+	mov x1, 40
+	mov x2, 7
+	add x3, x17, 10
+	sub x4, x18, 7
+	movz w10, 0xD0D0, lsl 0
+	movk w10, 0x00D0, lsl 16
+	bl rectangulo
+
+	mov x1, 15
+	mov x2, 8
+	add x3, x17, 20
+	sub x4, x18, 12 
+        movz w10, 0xD0D0, lsl 0
+        movk w10, 0x00D0, lsl 16
+        bl rectangulo
+        
+       ldr x30, [sp, 0]
+       add sp, sp, 8
+
+	ret
+
+dibujar_fondo:
+
+ sub sp, sp, 8
+ stur x30, [sp, 0]
+
+        // --------- FONDO CELESTE
+movz x1, 640, lsl 0        // ancho 
+movz x2, 400, lsl 0        // altura
+movz x3, 0, lsl 0          // x inicial
+movz x4, 0, lsl 0          // y inicial
+movz w10, 0xFFFF, lsl 0    // color cielo
+bl rectangulo              // llama a la función
+
+// --------- PASTO VERDE CLARO --
+movz x1, 640, lsl 0        // ancho 
+movz x2, 80, lsl 0         // altura         
+movz x3, 0, lsl 0          // x inicial
+movz x4, 400, lsl 0        // y inicial
+movz w10, 0xFF00, lsl 0    // color
+bl rectangulo
+
+
+// --------- PASTO VERDE MEDIO --
+movz x1, 640, lsl 0        // ancho
+movz x2, 40, lsl 0         // altura
+movz x3, 0, lsl 0          // x inicial
+movz x4, 420, lsl 0        // y inicial
+movz w10, 0x8000, lsl 0    // color
+bl rectangulo
+
+// --------- PASTO VERDE MEDIO (detalles)--
+movz x1, 50, lsl 0        // ancho
+movz x2, 10, lsl 0         // altura
+movz x3, 0, lsl 0          // x inicial
+movz x4, 410, lsl 0        // y inicial
+bl rectangulo
+
+movz x1, 53, lsl 0        // ancho
+movz x2, 5, lsl 0         // altura
+movz x3, 0, lsl 0          // x inicial
+movz x4, 415, lsl 0        // y inicial
+bl rectangulo
+
+movz x1, 44, lsl 0        // ancho
+movz x2, 5, lsl 0         // altura
+movz x3, 0, lsl 0          // x inicial
+movz x4, 405, lsl 0        // y inicial
+bl rectangulo
+
+// --------- PASTO VERDE OSCURO --
+movz x1, 640, lsl 0        // ancho
+movz x2, 30, lsl 0         // altura
+movz x3, 0, lsl 0          // x inicial
+movz x4, 450, lsl 0        // y inicial
+movz w10, 0x6000, lsl 0    // color
+bl rectangulo
+
+// --------- PASTO VERDE CLARO DEL MEDIO --
+movz x1, 340, lsl 0        // ancho
+movz x2, 15, lsl 0         // altura
+movz x3, 150, lsl 0          // x inicial
+movz x4, 420, lsl 0        // y inicial
+movz w10, 0xFF00, lsl 0    // color
+bl rectangulo
+
+// --------- PASTO VERDE CLARO DEL MEDIO (detalles)--
+movz x1, 5, lsl 0        // ancho
+movz x2, 5, lsl 0         // altura
+movz x3, 145, lsl 0          // x inicial
+movz x4, 420, lsl 0        // y inicial
+bl rectangulo
+
+// --------- PASTO VERDE CLARO DEL MEDIO (detalles)--
+movz x1, 5, lsl 0        // ancho
+movz x2, 5, lsl 0         // altura
+movz x3, 490, lsl 0          // x inicial
+movz x4, 420, lsl 0        // y inicial
+bl rectangulo
+
+// --------- PASTO VERDE OSCURO (detalles) --
+movz x1, 50, lsl 0        // ancho
+movz x2, 20, lsl 0         // altura
+movz x3, 0, lsl 0          // x inicial
+movz x4, 440, lsl 0        // y inicial
+movz w10, 0x6000, lsl 0    // color
+bl rectangulo
+
+movz x1, 25, lsl 0        // ancho
+movz x2, 10, lsl 0         // altura
+movz x3, 0, lsl 0          // x inicial
+movz x4, 430, lsl 0        // y inicial
+bl rectangulo
+
+movz x1, 50, lsl 0        // ancho
+movz x2, 20, lsl 0         // altura
+movz x3, 590, lsl 0          // x inicial
+movz x4, 440, lsl 0        // y inicial
+bl rectangulo
+
+movz x1, 25, lsl 0        // ancho
+movz x2, 10, lsl 0         // altura
+movz x3, 615, lsl 0          // x inicial
+movz x4, 430, lsl 0        // y inicial
+bl rectangulo
+
+// --------- PASTO VERDE MEDIO (detalles) --
+movz x1, 50, lsl 0        // ancho
+movz x2, 10, lsl 0         // altura
+movz x3, 590, lsl 0          // x inicial
+movz x4, 410, lsl 0        // y inicial
+movz w10, 0x8000, lsl 0    // color
+bl rectangulo
+
+movz x1, 53, lsl 0        // ancho
+movz x2, 5, lsl 0         // altura
+movz x3, 587, lsl 0          // x inicial
+movz x4, 415, lsl 0        // y inicial
+bl rectangulo
+
+movz x1, 44, lsl 0        // ancho
+movz x2, 5, lsl 0         // altura
+movz x3, 596, lsl 0          // x inicial
+movz x4, 405, lsl 0        // y inicial
+bl rectangulo
+
+// -------PODIO --
+
+// ---------- SOMBRA --
+movz x1, 7, lsl 0       // ancho
+movz x2, 95, lsl 0       // alto
+movz x3, 243, lsl 0      // x
+movz x4, 315, lsl 0      // y
+movz w10, 0x5050, lsl 0
+movk w10, 0x0050, lsl 16
+bl rectangulo
+
+// ---------- PODIO IZQUIERDA--
+movz x1, 75, lsl 0       // ancho
+movz x2, 60, lsl 0       // alto
+movz x3, 170, lsl 0      // x
+movz x4, 350, lsl 0      // y
+movz w10, 0xAAAA, lsl 0
+movk w10, 0x00AA, lsl 16
+bl rectangulo
+
+// ---------- PODIO GRANDE--
+movz x1, 125, lsl 0       // ancho
+movz x2, 95, lsl 0       // alto
+movz x3, 250, lsl 0      // x
+movz x4, 315, lsl 0      // y
+bl rectangulo 
+
+// ------- PALO --
+movz x1, 8, lsl 0        // ancho
+movz x2, 135, lsl 0         // altura
+movz x3, 250, lsl 0          // x inicial
+movz x4, 180, lsl 0        // y inicial
+movz w10, 0x4513, lsl 0    // MARRON
+movk w10, 0x008B, lsl 16    // 
+bl rectangulo
+
+//-------PALO IZQUIERDA (el ancho, altura y color ya quedaron guardados en los registros en la llamada anterior)-------
+movz x3, 173, lsl 0          // x inicial
+movz x4, 215, lsl 0        // y inicial
+bl rectangulo
+
+//--------- BANDERA ARG -----------
+
+movz x1, 100, lsl 0
+movz x2, 45, lsl 0
+movz x3, 258,lsl 0
+movz x4, 185,lsl 0
+MOVZ w10, 0xC3F7, lsl 0  //  CELESTE         
+MOVK w10, 0x004F, lsl 16 
+bl rectangulo
+
+movz x1, 100,lsl 0
+movz x2, 15, lsl 0
+movz x3, 258,lsl 0
+movz x4, 200,lsl 0
+movz w10, 0xFFFF, lsl 0    //  BLANCO    
+movk w10, 0x00FF, lsl 16 
+bl rectangulo
+
+movz x1, 9,lsl 0
+movz x2, 9,lsl 0
+movz x3, 302,lsl 0
+movz x4, 203,lsl 0
+MOVZ w10, 0xFF99, lsl 0  // AMARILLO        
+MOVK w10, 0x00FF, lsl 16  
+bl rectangulo
+
+//-----BANDERA FRANCIA------
+
+movz x1 , 26 ,lsl 0
+movz x2 , 45 ,lsl 0
+movz x3 , 180, lsl 0
+movz x4 , 218, lsl 0
+MOVZ w10, 0x55A4, lsl 0    // Color azul
+MOVK w10, 0x0000, lsl 16  // 
+bl rectangulo
+
+movz x1 , 26 ,lsl 0
+movz x2 , 45 ,lsl 0
+movz x3 , 206, lsl 0
+movz x4 , 218, lsl 0
+MOVZ w10, 0xFFFF, lsl 0
+MOVK w10, 0x00FF, lsl 16 // Color Blanco 
+bl rectangulo
+
+movz x1 , 26 ,lsl 0
+movz x2 , 45 ,lsl 0
+movz x3 , 232, lsl 0
+movz x4 , 218, lsl 0
+MOVZ w10, 0x4135, lsl 0    // Color Rojo
+MOVK w10, 0x00EF, lsl 16  //  
+bl rectangulo
+
+// --- ODC 2025 --
+
+// -- O --
+movz w10, 0x0000, lsl 0
+movk w10, 0x0000, lsl 16
+
+// -- LINEA SUPERIOR  --
+movz x1, 8, lsl 0
+movz x2, 2, lsl 0
+movz x3, 188, lsl 0
+movz x4, 365, lsl 0
+bl rectangulo
+
+// --- LINEA INFERIOR ---
+movz x3, 188, lsl 0
+movz x4, 389, lsl 0
+bl rectangulo
+
+// -- LINEA IZQUIERDA -- 
+movz x1, 2, lsl 0
+movz x2, 24, lsl 0
+movz x3, 194, lsl 0
+movz x4, 365, lsl 0
+bl rectangulo
+
+// -- LINEA DERECHA --
+movz x3, 188, lsl 0
+movz x4, 365, lsl 0
+bl rectangulo
+
+// -- D -- 
+
+// -- LINEA IZQUIERDA --
+movz x1, 2, lsl 0
+movz x2, 26, lsl 0
+movz x3, 204, lsl 0
+movz x4, 365, lsl 0
+bl rectangulo
+
+// --- LINEA SUPERIOR ---
+movz x1, 7, lsl 0
+movz x2, 2, lsl 0
+movz x3, 206, lsl 0
+movz x4, 365, lsl 0
+bl rectangulo
+
+// --- LINEA INFERIOR ---
+movz x3, 206, lsl 0
+movz x4, 389, lsl 0
+bl rectangulo
+
+// -- LINEA DERECHA --
+movz x1, 2, lsl 0
+movz x2, 23, lsl 0
+movz x3, 212, lsl 0
+movz x4, 366, lsl 0
+bl rectangulo
+
+// -- C -- 
+
+// --- LINEA IZQUIERDA ---
+movz x1, 2, lsl 0
+movz x2, 25, lsl 0
+movz x3, 222, lsl 0
+movz x4, 366, lsl 0
+bl rectangulo
+
+// --- LINEA SUPERIOR ---
+movz x1, 7, lsl 0
+movz x2, 2, lsl 0
+movz x3, 224, lsl 0
+movz x4, 366, lsl 0
+bl rectangulo
+
+// --- LINEA INFERIOR ---
+movz x3, 224, lsl 0
+movz x4, 389, lsl 0
+bl rectangulo
+
+// --- 2 ----
+
+// --- LÍNEA SUPERIOR ---
+movz x1, 20, lsl 0        
+movz x2, 2, lsl 0        
+movz x3, 257, lsl 0       
+movz x4, 366, lsl 0      
+bl rectangulo
+
+// --- LÍNEA MEDIA ---
+movz x3, 257, lsl 0
+movz x4, 377, lsl 0
+bl rectangulo
+
+// --- LÍNEA INFERIOR ---
+movz x3, 257, lsl 0
+movz x4, 389, lsl 0
+bl rectangulo
+
+// --- LINEA DERECHA SUPERIOR  ---
+movz x1, 2, lsl 0
+movz x2, 10, lsl 0
+movz x3, 275, lsl 0     
+movz x4, 367, lsl 0
+bl rectangulo
+
+// --- LINEA DERECHA IZQUIERDA ---
+movz x3, 257, lsl 0
+movz x4, 379, lsl 0
+bl rectangulo
+
+// --- 0 ---
+
+// --- LÍNEA SUPERIOR ---
+movz x1, 20, lsl 0
+movz x2, 2, lsl 0
+movz x3, 287, lsl 0      
+movz x4, 366, lsl 0
+bl rectangulo
+
+// --- LÍNEA INFERIOR ---
+
+movz x3, 287, lsl 0
+movz x4, 389, lsl 0
+bl rectangulo
+
+// --- LADO IZQUIERDO ---
+movz x1, 2, lsl 0
+movz x2, 22, lsl 0     
+movz x3, 287, lsl 0
+movz x4, 367, lsl 0
+bl rectangulo
+
+// --- LADO DERECHO ---
+movz x3, 305, lsl 0
+movz x4, 367, lsl 0
+bl rectangulo
+
+// --- 2 ---
+
+// --- LÍNEA SUPERIOR ---
+movz x1, 20, lsl 0
+movz x2, 2, lsl 0
+movz x3, 317, lsl 0
+movz x4, 366, lsl 0
+bl rectangulo
+
+// --- LÍNEA MEDIA ---
+movz x3, 317, lsl 0
+movz x4, 377, lsl 0
+bl rectangulo
+
+// --- LÍNEA INFERIOR ---
+movz x3, 317, lsl 0
+movz x4, 389, lsl 0
+bl rectangulo
+
+// --- LINEA DERECHA IZQUIERDA ---
+movz x1, 2, lsl 0
+movz x2, 10, lsl 0
+movz x3, 317, lsl 0
+movz x4, 379, lsl 0
+bl rectangulo
+
+// --- LINEA DERECHA SUPERIOR  ---
+movz x3, 335, lsl 0
+movz x4, 367, lsl 0
+bl rectangulo
+
+// --- 5 ---
+
+// --- LÍNEA SUPERIOR ---
+movz x1, 20, lsl 0
+movz x2, 2, lsl 0
+movz x3, 347, lsl 0       
+movz x4, 366, lsl 0
+bl rectangulo
+
+// --- LÍNEA MEDIA ---
+movz x3, 347, lsl 0
+movz x4, 377, lsl 0
+bl rectangulo
+
+// --- LÍNEA INFERIOR ---
+movz x3, 347, lsl 0
+movz x4, 389, lsl 0
+bl rectangulo
+
+// --- LINEA IZQUIERDA ---
+movz x1, 2, lsl 0
+movz x2, 10, lsl 0
+movz x3, 347, lsl 0
+movz x4, 367, lsl 0
+bl rectangulo
+
+// --- LINEA DERECHA ---
+movz x3, 365, lsl 0      
+movz x4, 379, lsl 0
+bl rectangulo
+
+  ldr x30, [sp, 0]
+  add sp, sp, 8
+ret
+
